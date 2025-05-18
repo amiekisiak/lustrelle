361965c0f23c519f866a319f3508b24d90b3c942
amiekisiak <amiekisiak@gmail.com>
2025-05-18 21:42:18 +0100

    added new picture

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
-  });
+  })
+
+   function openModal(src) {
+      const modal = document.getElementById("imageModal");
+      const modalImg = document.getElementById("modalImg");
+      modal.style.display = "block";
+      modalImg.src = src;
+    }
+
+    function closeModal() {
+      document.getElementById("imageModal").style.display = "none";
+    }
